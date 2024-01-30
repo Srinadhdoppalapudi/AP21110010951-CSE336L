8a1513c35a2fc349337a135edd559b85eb91119d
SRINADH DOPPALAPUDI <114092699+Srinadhdoppalapudi@users.noreply.github.com>
2024-01-30 23:12:31 +0530

    Update Question8.py

--- Week1-Pandas/Question8.py
+++ Week1-Pandas/Question8.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-exam_data  = {'name': ['Srinadh', 'Sai', 'Sampath', 'Nandu', 'Ashokh', 'Sai Krishna', 'King', 'Lara', 'john', 'Banu'],
+exam_data  = {'name': ['Srinadh', 'Sai', 'Sampath', 'Nandu', 'Ashokh', 'Sai Krishna', 'King', 'niteesh', 'john', 'Banu'],
         'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
         'attempts' : [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
         'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}

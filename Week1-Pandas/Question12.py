72c70ec5ae99a6eb33eb02fa4a58590439fda085
SRINADH DOPPALAPUDI <114092699+Srinadhdoppalapudi@users.noreply.github.com>
2024-01-30 08:23:49 -0800

    Update Question12.py

--- Week1-Pandas/Question12.py
+++ Week1-Pandas/Question12.py
@@ -1,7 +1,6 @@
 import pandas as pd 
 import numpy as np 
-dict = {'Name': ['Dhoni', 'Kohli', 'Surya', 
-				'Samson', 'Jaiswal'], 
+dict = {'Name': ['Dhoni', 'Kohli', 'Surya', 'Samson', 'Jaiswal'], 
 		'Age': [22, 20, np.inf, -np.inf, 22], 
 		'Marks': [90, 84, 33, 87, 82]} 
 
